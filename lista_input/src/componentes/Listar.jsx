64235a618047271bb7d3c1fde2d2ba45800d751d
késsia <ylannekessia29@gmail.com>
2025-07-01 10:45:22 -0300

    atividade de fixação

--- lista_input/src/componentes/Listar.jsx
+++ lista_input/src/componentes/Listar.jsx
@@ -1,34 +1,59 @@
-import { useState } from "react"
+import { useState } from 'react'
+import './Listar.css'
 
-export default function Listar() {
-    const [tarefa, setTarefa] = useState ('')
+export default function ListaDeTarefas() {
+    const [tarefa, setTarefa] = useState('')
     const [lista, setLista] = useState([])
 
     const handleSubmit = (e) => {
-        e.preventDefault();
-        setLista([...lista, tarefa])
+        e.preventDefault()
+        setLista([...lista, { texto: tarefa, status: 'pendente' }])
         setTarefa('')
+    }
+    const mudarStatus = (i, status) => {
+        const novaLista = [...lista]
+        novaLista[i].status = status
+        setLista(novaLista)
+    }
+    const organizarTarefas = (i, lugar) => {
+        const novaLista = [...lista]
+        const novaPosicao = i + lugar
 
+        if (novaPosicao < 0) return
+        if (novaPosicao >= lista.length) return
+
+        const auxiliar = novaLista[novaPosicao]
+        novaLista[novaPosicao] = novaLista[i]
+        novaLista[i] = auxiliar
+
+        setLista(novaLista)
     }
 
-    return(
-        <div>
-            <h2>Lista de Tarefas React</h2>
+  return (
+    <div className="lista">
+      <h2>Lista de Tarefas</h2>
 
-            <form onSubmit={handleSubmit}>
-                <label>
-                    <input type="text" name="tarefa" value={tarefa} onChange={(e) => setTarefa(e.target.value)} />
-                </label>
-                    <input type="submit" value="Adicionar" />
+        <form onSubmit={handleSubmit}>
+            <label>
+                <input type="text" name="tarefa" value={tarefa} onChange={(e) => setTarefa(e.target.value)} />
+            </label>
+                <input type="submit" value="Adicionar" />
+        </form>
 
-            </form>
-
-            <ul>
-                {lista.map((item) =>
-                <li>{item}</li>
-            )}
-            </ul>
-
-        </div>
-    )
+      {lista.map((item, i) => (
+        <li key={i} className={item.status === 'realizada' ? 'concluida' : item.status === 'nao-realizada' ? 'nao-realizada' : 'pendente'}>
+            <div>
+                <span>{item.texto }</span>
+                <div className="botoes-status">
+                    <button type="button" className={item.status === 'realizada' ? 'realizada' : ''} onClick={() => mudarStatus(i, 'realizada')}>Realizada</button>
+                    <button type="button" className={item.status === 'nao-realizada' ? 'nao-realizada' : ''} onClick={() => mudarStatus(i, 'nao-realizada')}>Não realizada</button>
+                    <button type="button" className={item.status === 'pendente' ? 'pendente' : ''} onClick={() => mudarStatus(i, 'pendente')}>Pendente</button>
+                    <button id='bt-posicao' onClick={() => organizarTarefas(i, -1)} >p/ cima</button>
+                    <button id='bt-posicao' onClick={() => organizarTarefas(i, +1)} >p/ baixo</button>
+                </div>
+            </div>
+        </li>
+      ))}
+    </div>
+  )
 }
